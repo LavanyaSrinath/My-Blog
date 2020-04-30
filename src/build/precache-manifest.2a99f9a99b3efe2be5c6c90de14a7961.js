40294a733f530e2cdccc1af452712282d7895477
rajivlovespeterscamel <51520079+rajivlovespeterscamel@users.noreply.github.com>
2020-04-29 22:40:29 -0400

    First commit

---
+++ src/build/precache-manifest.2a99f9a99b3efe2be5c6c90de14a7961.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9948c687fd293f88cb5",
+    "url": "/static/css/main.4fb36efb.chunk.css"
+  },
+  {
+    "revision": "f9948c687fd293f88cb5",
+    "url": "/static/js/main.58152645.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3411b17d3915443b45c",
+    "url": "/static/js/2.4a7f71bf.chunk.js"
+  },
+  {
+    "revision": "3f980b78638b4dfc3d5e681a2ad4ffc4",
+    "url": "/index.html"
+  }
+];
